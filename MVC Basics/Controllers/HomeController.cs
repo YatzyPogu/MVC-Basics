55095039b862f1750d515aa9b5c2c672b675cb02
Ola Ulvås <olaulvas@hotmail.com>
2020-01-02 13:14:33 +0100

    Assignment1

--- MVC Basics/Controllers/HomeController.cs
+++ MVC Basics/Controllers/HomeController.cs
@@ -8,6 +8,10 @@ namespace MVC_Basics.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
         public IActionResult About()
         {
             return View();
